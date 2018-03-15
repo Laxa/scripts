acdac96dc87fb5ad02492ca142aea392d91dcc0a
laxa <laxa@ddracepro.net>
2018-03-15 14:49:43 +0100

    Improved previous script

--- misc/test_sockets_client.py
+++ misc/test_sockets_client.py
@@ -1,7 +1,6 @@
 import socket
 
 IP = 'x.x.x.x'
-MESSAGE = 'Hello, World!\n'
 DEBUG = True
 ports = [21, 22, 53, 25, 111, 443, 8443, 80, 8889, 10000, 21050, 48000, 48001, 14000]
 socket.setdefaulttimeout(5)
@@ -12,7 +11,8 @@ for port in ports:
     try:
         sock = socket.socket(socket.AF_INET, # Internet
                         socket.SOCK_DGRAM) # UDP
-        sock.sendto(MESSAGE.encode(), (IP, port))
+        message = 'UDP %d' % port
+        sock.sendto(message.encode(), (IP, port))
     except Exception as e:
         print(e)
 
@@ -23,7 +23,8 @@ for port in ports:
         sock = socket.socket(socket.AF_INET, # Internet
                         socket.SOCK_STREAM) # TCP
         sock.connect((IP, port))
-        sock.send(MESSAGE.encode())
+        message = 'TCP %d' % port
+        sock.send(message.encode())
         print('Port %d working' % port)
     except socket.timeout:
         if DEBUG:
