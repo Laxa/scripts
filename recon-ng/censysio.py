e4c9e68f911f588226be83ce6b381f23603104e1
laxa <laxa@ddracepro.net>
2019-08-20 15:45:51 +0200

    .

--- recon-ng/censysio.py
+++ recon-ng/censysio.py
@@ -5,12 +5,12 @@ class Module(BaseModule):
 
     meta = {
         'name': 'Censys.io Domain to hosts enumerator',
-        'author': 'laxa',
+        'author': 'laxa (@l4x4)',
         'description': 'Queries the censys.io API to find hosts based on domain.',
         'required_keys': ['censysio_id', 'censysio_secret'],
         'comments': (
             'Can also be used to target wildcard domains, to do so type:',
-            'set source *.domain.tld',
+            'options set SOURCE *.domain.tld',
         ),
         'query': 'SELECT DISTINCT domain FROM domains WHERE domain IS NOT NULL',
         'options': (
@@ -26,10 +26,10 @@ class Module(BaseModule):
             while True:
                 resp = self._get_page(domain, page)
                 if resp.status_code != 200:
-                    self.error('Error: \'%s\'' % (resp.json.get('error')))
+                    self.error('Error: \'%s\'' % (resp.json().get('error')))
                     break
                 self._load_results(resp)
-                if resp.json.get('metadata').get('page') >= resp.json.get('metadata').get('pages'):
+                if resp.json().get('metadata').get('page') >= resp.json().get('metadata').get('pages'):
                     break
                 self.verbose('Fetching the next page of results...')
                 page += 1
@@ -41,22 +41,21 @@ class Module(BaseModule):
             'fields': ['ip', 'protocols']
         }
         resp = self.request(
+			'POST',
             'https://censys.io/api/v1/search/ipv4',
-            payload=payload,
+            json=payload,
             auth=(
                 self.keys.get('censysio_id'),
                 self.keys.get('censysio_secret')
             ),
-            method='POST',
-            content='JSON',
         )
         if self.options['limit']:
             time.sleep(1 / self.options['rate'])
         return resp
 
     def _load_results(self, resp):
-        for result in resp.json.get('results'):
+        for result in resp.json().get('results'):
             ip_address = result.get('ip')
             for service in result.get('protocols'):
                 port, protocol = service.split('/')
-                self.add_ports(ip_address=ip_address, port=port, protocol=protocol)
+                self.insert_ports(ip_address=ip_address, port=port, protocol=protocol)
