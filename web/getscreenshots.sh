9d26b685c20eb278d599646b420f16f7b144305e
laxa <laxa@ddracepro.net>
2020-01-20 17:18:47 +0100

    .

--- web/getscreenshots.sh
+++ web/getscreenshots.sh
@@ -11,7 +11,8 @@ while read p; do
     if [ $port -eq 80 ]; then
         scheme='http'
     fi
-    timeout 3 google-chrome --headless --window-size=1920,1080 --ignore-certificate-errors --screenshot --proxy-server=http://127.0.0.1:8080 "${scheme}://${host}:${port}"
+#https://github.com/michenriksen/aquatone/blob/854a5d56fbb7a00b2e5ec80d443026c7a4ced798/agents/url_screenshotter.go#L127
+    timeout 3 google-chrome --headless --window-size=1920,1080 --ignore-certificate-errors --screenshot --incognito --disable-gpu --disable-crash-reporter --disable-notifications --no-first-run --disable-infobars --disable-sync --no-default-browser-check --proxy-server=http://127.0.0.1:8080 "${scheme}://${host}:${port}"
     mv "screenshot.png" out/"${host}_${port}.png"
     echo "${host}_${port}"
 done < $FILE
