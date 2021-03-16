9b2c8e3e01f83a863abefd159cfaef23cad91b0b
laxa <laxa@ddracepro.net>
2021-03-16 18:38:28 +0100

    File upload python3

--- web/SimpleHTTPServerWithUpload.py
+++ web/SimpleHTTPServerWithUpload.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
 # script sources: https://gist.github.com/UniIsland/3346170
 
@@ -17,19 +17,16 @@ __home_page__ = "http://li2z.cn/"
 
 import os
 import posixpath
-import BaseHTTPServer
+import http.server
 import urllib
-import cgi
+import html
 import shutil
 import mimetypes
 import re
-try:
-    from cStringIO import StringIO
-except ImportError:
-    from StringIO import StringIO
+import io
 
 
-class SimpleHTTPRequestHandler(BaseHTTPServer.BaseHTTPRequestHandler):
+class SimpleHTTPRequestHandler(http.server.BaseHTTPRequestHandler):
 
     """Simple HTTP request handler with GET/HEAD/POST commands.
 
@@ -61,21 +58,22 @@ class SimpleHTTPRequestHandler(BaseHTTPServer.BaseHTTPRequestHandler):
     def do_POST(self):
         """Serve a POST request."""
         r, info = self.deal_post_data()
-        print r, info, "by: ", self.client_address
-        f = StringIO()
-        f.write('<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 3.2 Final//EN">')
-        f.write("<html>\n<title>Upload Result Page</title>\n")
-        f.write("<body>\n<h2>Upload Result Page</h2>\n")
-        f.write("<hr>\n")
+        print(r, info, "by: ", self.client_address)
+        info = info.encode()
+        f = io.BytesIO()
+        f.write(b'<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 3.2 Final//EN">')
+        f.write(b'<html>\n<title>Upload Result Page</title>\n')
+        f.write(b'<body>\n<h2>Upload Result Page</h2>\n')
+        f.write(b'<hr>\n')
         if r:
-            f.write("<strong>Success:</strong>")
+            f.write(b'<strong>Success:</strong>')
         else:
-            f.write("<strong>Failed:</strong>")
+            f.write(b'<strong>Failed:</strong>')
         f.write(info)
-        f.write("<br><a href=\"%s\">back</a>" % self.headers['referer'])
-        f.write("<hr><small>Powerd By: bones7456, check new version at ")
-        f.write("<a href=\"http://li2z.cn/?s=SimpleHTTPServerWithUpload\">")
-        f.write("here</a>.</small></body>\n</html>\n")
+        f.write(b'<br><a href=\"%s\">back</a>' % self.headers['referer'].encode())
+        f.write(b'<hr><small>Powerd By: bones7456, check new version at ')
+        f.write(b'<a href=\"http://li2z.cn/?s=SimpleHTTPServerWithUpload\">')
+        f.write(b'here</a>.</small></body>\n</html>\n')
         length = f.tell()
         f.seek(0)
         self.send_response(200)
@@ -87,15 +85,16 @@ class SimpleHTTPRequestHandler(BaseHTTPServer.BaseHTTPRequestHandler):
             f.close()
         
     def deal_post_data(self):
-        boundary = self.headers.plisttext.split("=")[1]
+        boundary = self.headers['Content-Type'].split("=")[1].encode()
         remainbytes = int(self.headers['content-length'])
         line = self.rfile.readline()
         remainbytes -= len(line)
         if not boundary in line:
             return (False, "Content NOT begin with boundary")
+        import pdb; pdb.set_trace()
         line = self.rfile.readline()
         remainbytes -= len(line)
-        fn = re.findall(r'Content-Disposition.*name="file"; filename="(.*)"', line)
+        fn = re.findall(r'Content-Disposition.*name="file"; filename="(.*)"', line.decode())
         if not fn:
             return (False, "Can't find out file name...")
         path = self.translate_path(self.path)
@@ -116,7 +115,7 @@ class SimpleHTTPRequestHandler(BaseHTTPServer.BaseHTTPRequestHandler):
             remainbytes -= len(line)
             if boundary in line:
                 preline = preline[0:-1]
-                if preline.endswith('\r'):
+                if preline.endswith(b'\r'):
                     preline = preline[0:-1]
                 out.write(preline)
                 out.close()
@@ -184,16 +183,16 @@ class SimpleHTTPRequestHandler(BaseHTTPServer.BaseHTTPRequestHandler):
             self.send_error(404, "No permission to list directory")
             return None
         list.sort(key=lambda a: a.lower())
-        f = StringIO()
-        displaypath = cgi.escape(urllib.unquote(self.path))
-        f.write('<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 3.2 Final//EN">')
-        f.write("<html>\n<title>Directory listing for %s</title>\n" % displaypath)
-        f.write("<body>\n<h2>Directory listing for %s</h2>\n" % displaypath)
-        f.write("<hr>\n")
-        f.write("<form ENCTYPE=\"multipart/form-data\" method=\"post\">")
-        f.write("<input name=\"file\" type=\"file\"/>")
-        f.write("<input type=\"submit\" value=\"upload\"/></form>\n")
-        f.write("<hr>\n<ul>\n")
+        f = io.BytesIO()
+        displaypath = html.escape(urllib.parse.unquote(self.path))
+        f.write(b'<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 3.2 Final//EN">')
+        f.write(b'<html>\n<title>Directory listing for %s</title>\n' % displaypath.encode())
+        f.write(b'<body>\n<h2>Directory listing for %s</h2>\n' % displaypath.encode())
+        f.write(b'<hr>\n')
+        f.write(b'<form ENCTYPE=\"multipart/form-data\" method=\"post\">')
+        f.write(b'<input name=\"file\" type=\"file\"/>')
+        f.write(b'<input type=\"submit\" value=\"upload\"/></form>\n')
+        f.write(b'<hr>\n<ul>\n')
         for name in list:
             fullname = os.path.join(path, name)
             displayname = linkname = name
@@ -204,9 +203,9 @@ class SimpleHTTPRequestHandler(BaseHTTPServer.BaseHTTPRequestHandler):
             if os.path.islink(fullname):
                 displayname = name + "@"
                 # Note: a link to a directory displays with @ and links with /
-            f.write('<li><a href="%s">%s</a>\n'
-                    % (urllib.quote(linkname), cgi.escape(displayname)))
-        f.write("</ul>\n<hr>\n</body>\n</html>\n")
+            f.write(b'<li><a href="%s">%s</a>\n'
+                    % (urllib.parse.quote(linkname).encode(), html.escape(displayname).encode()))
+        f.write(b'</ul>\n<hr>\n</body>\n</html>\n')
         length = f.tell()
         f.seek(0)
         self.send_response(200)
@@ -226,7 +225,7 @@ class SimpleHTTPRequestHandler(BaseHTTPServer.BaseHTTPRequestHandler):
         # abandon query parameters
         path = path.split('?',1)[0]
         path = path.split('#',1)[0]
-        path = posixpath.normpath(urllib.unquote(path))
+        path = posixpath.normpath(urllib.parse.unquote(path))
         words = path.split('/')
         words = filter(None, words)
         path = os.getcwd()
@@ -289,8 +288,8 @@ class SimpleHTTPRequestHandler(BaseHTTPServer.BaseHTTPRequestHandler):
 
 
 def test(HandlerClass = SimpleHTTPRequestHandler,
-         ServerClass = BaseHTTPServer.HTTPServer):
-    BaseHTTPServer.test(HandlerClass, ServerClass)
+         ServerClass = http.server.HTTPServer):
+    http.server.test(HandlerClass, ServerClass)
 
 if __name__ == '__main__':
     test()
